9dc20404906fa78309660658d850ea8936de5156
vcosmin2701 <v.cosmin2701@gmail.com>
2024-07-09 19:05:17 +0300

    Logo added to the Mobile header

--- src/components/mobileHeader/header.jsx
+++ src/components/mobileHeader/header.jsx
@@ -1,14 +1,29 @@
-'use client'
+'use client';
 
-import { useState } from "react"
-import { Overlay } from "./overlay";
-import { BurgerSvg } from "./burgerSvg";
+import { useState } from 'react';
+import { Overlay } from './overlay';
+import { BurgerSvg } from './burgerSvg';
+import Image from 'next/image';
 
 export const MobileHeader = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    return <div className="block lg:hidden p-5">
-        <button onClick={() => setIsOpen(true)}><BurgerSvg /></button>
-        <Overlay setIsOpen={setIsOpen} isOpen={isOpen}/>
-    </div>
-}
+  return (
+    <>
+      <div className="lg:hidden p-5 flex justify-between items-center">
+        <button onClick={() => setIsOpen(true)}>
+          <BurgerSvg />
+        </button>
+        <div className="mt-2">
+          <Image
+            src="/flitnext-logo.png"
+            alt="FlitNext Logo"
+            width={120}
+            height={120}
+          />
+        </div>
+        <Overlay setIsOpen={setIsOpen} isOpen={isOpen} />
+      </div>
+    </>
+  );
+};
