6a807811575bf19df5ed6482331f088fda5a74fb
vcosmin2701 <v.cosmin2701@gmail.com>
2024-07-09 12:39:18 +0300

    SVGs extracted separately

--- src/components/mobileHeader/header.jsx
+++ src/components/mobileHeader/header.jsx
@@ -1,17 +1,18 @@
-'use client'
+'use client';
 
-import { useState } from "react"
-import { Overlay } from "./overlay";
+import { useState } from 'react';
+import { Overlay } from './overlay';
+import { HamburgerIcon } from '@/assets/icons/hamburgerIcon';
 
 export const MobileHeader = () => {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    return <div className="block lg:hidden p-5">
-        <button onClick={() => setIsOpen(true)}>
-            <svg className="w-7 h-7 text-red-50" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
-            </svg>
-        </button>
-        <Overlay setIsOpen={setIsOpen} isOpen={isOpen}/>
+  return (
+    <div className="block lg:hidden p-5">
+      <button onClick={() => setIsOpen(true)}>
+        <HamburgerIcon />
+      </button>
+      <Overlay setIsOpen={setIsOpen} isOpen={isOpen} />
     </div>
-}
+  );
+};
--- src/components/mobileHeader/overlay.jsx
+++ src/components/mobileHeader/overlay.jsx
@@ -1,17 +1,13 @@
+import { OverlayIcon } from '@/assets/icons/overlayIconX';
 
-export const Overlay = ({ setIsOpen, isOpen }) => <div className={`absolute left-0 top-0 h-full bg-[#C5C6C7] w-full ${isOpen ? "block" : "hidden"} z-10`}>
-<button onClick={() => setIsOpen(false)}>
-<svg viewBox="0 0 12 12" version="1.1"
-    xmlns="http://www.w3.org/2000/svg" className="text-[#1F2833] w-7 h-7 m-5">
-    <line x1="1" y1="11" 
-        x2="11" y2="1" 
-        stroke="black" 
-        strokeWidth="2"/>
-    <line x1="1" y1="1" 
-        x2="11" y2="11" 
-        stroke="black" 
-        strokeWidth="2"/>
-</svg>
-</button>
-
-</div>
+export const Overlay = ({ setIsOpen, isOpen }) => (
+  <div
+    className={`absolute left-0 top-0 h-full bg-[#C5C6C7] w-full ${
+      isOpen ? 'block' : 'hidden'
+    } z-10`}
+  >
+    <button onClick={() => setIsOpen(false)}>
+      <OverlayIcon />
+    </button>
+  </div>
+);
