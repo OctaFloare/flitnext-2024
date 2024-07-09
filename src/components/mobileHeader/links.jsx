6050c4453ac100b9c8bd4b5b59bc2a8062936eda
Cepoiu Radu Konstantin <radu.cepoiu@gmail.com>
2024-07-09 23:27:40 +0300

    rounded mobile menu entries

--- src/components/mobileHeader/links.jsx
+++ src/components/mobileHeader/links.jsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
 export const Links = ({ setIsOpen }) => <div className="flex flex-col gap-2">
-    <Link href="/" className="bg-slate-900 pl-10 h-12 flex items-center ml-5 mr-5" onClick={() => setIsOpen(false)}>Home</Link>
-    <Link href="/movies" className=" bg-slate-900 pl-10  h-12 flex items-center ml-5 mr-5" onClick={() => setIsOpen(false)}>Movies</Link>
+    <Link href="/" className="bg-slate-900 pl-10 h-12 flex items-center ml-5 mr-5 rounded-2xl" onClick={() => setIsOpen(false)}>Home</Link>
+    <Link href="/movies" className=" bg-slate-900 pl-10  h-12 flex items-center ml-5 mr-5 rounded-2xl" onClick={() => setIsOpen(false)}>Movies</Link>
 </div>
