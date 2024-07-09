2d8e0683b88013c533f26e1e453b51122a562eed
Hezanin <hezanina@gmail.com>
2024-07-09 19:14:24 +0300

    Add new background and minor adjustments to navbars
    Update background image;
    Update desktop nav bar to not contain a background color and left nav bar links to pure white;
    Update mobile/tablet nav bar overlay to pure white and removed background from nav bar links;

--- src/components/mobileHeader/overlay.jsx
+++ src/components/mobileHeader/overlay.jsx
@@ -1,8 +1,10 @@
 import { BurgerSvg, XIconSvg } from "./xIconSvg";
+import { Links } from "./links";
 
 export const Overlay = ({ setIsOpen, isOpen }) => 
-<div className={`absolute left-0 top-0 h-full bg-[#C5C6C7] w-full ${isOpen ? "block" : "hidden"} z-10`}>
+<div className={`absolute left-0 top-0 h-full bg-[#FFF] w-full ${isOpen ? "block" : "hidden"} z-10`}>
     <button onClick={() => setIsOpen(false)}>
         <XIconSvg />
     </button>
+    <Links setIsOpen={setIsOpen}></Links>
 </div>
