2419ef579ca1dd2a6c9ef4aa97c9a03680d69325
vcosmin2701 <v.cosmin2701@gmail.com>
2024-07-09 18:58:31 +0300

    Logo added to the Desktop header

--- src/components/desktopHeader.jsx
+++ src/components/desktopHeader.jsx
@@ -1,10 +1,29 @@
-import Link from "next/link";
+import Link from 'next/link';
+import Image from 'next/image';
 
-export const DesktopHeader = () => <div className="hidden lg:block ">
-<nav
- className="w-full flex justify-center items-center gap-4 bg-[#C5C6C7] h-20"
- >
-    <Link href="/" className="text-2xl text-[#1F2833]">Home</Link>
-    <Link href="/movies" className="text-2xl text-[#1F2833]">Movies</Link>
-</nav>
-</div>
+export const DesktopHeader = () => (
+  <div className="hidden lg:block">
+    <nav className="w-full flex items-center bg-[#C5C6C7] h-20 px-10">
+      <div className="flex items-center justify-start">
+        <Image
+          src="/flitnext-logo.png"
+          alt="FlitNext Logo"
+          width={150}
+          height={150}
+        />
+      </div>
+      <div className="flex-grow flex items-center justify-center gap-5">
+        <Link href="/" className="text-2xl text-[#1F2833]">
+          Home
+        </Link>
+        <Link href="/movies" className="text-2xl text-[#1F2833]">
+          Movies
+        </Link>
+      </div>
+      <div
+        className="flex items-center justify-end"
+        style={{ width: 150 }}
+      ></div>
+    </nav>
+  </div>
+);
--- src/components/mobileHeader/overlay.jsx
+++ src/components/mobileHeader/overlay.jsx
@@ -1,8 +1,13 @@
-import { BurgerSvg, XIconSvg } from "./xIconSvg";
+import { BurgerSvg, XIconSvg } from './xIconSvg';
 
-export const Overlay = ({ setIsOpen, isOpen }) => 
-<div className={`absolute left-0 top-0 h-full bg-[#C5C6C7] w-full ${isOpen ? "block" : "hidden"} z-10`}>
+export const Overlay = ({ setIsOpen, isOpen }) => (
+  <div
+    className={`absolute left-0 top-0 h-full bg-[#C5C6C7] w-full ${
+      isOpen ? 'block' : 'hidden'
+    } z-10`}
+  >
     <button onClick={() => setIsOpen(false)}>
-        <XIconSvg />
+      <XIconSvg />
     </button>
-</div>
+  </div>
+);
