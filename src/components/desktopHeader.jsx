1e3ca36a6e66a76023d36861e135bfad0e5cc9ee
Cepoiu Radu Konstantin <radu.cepoiu@gmail.com>
2024-07-09 22:41:18 +0300

    Added icon to navbar desktop

--- src/components/desktopHeader.jsx
+++ src/components/desktopHeader.jsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
+import { FlixnetSvg } from "./mobileHeader/flixnetSvg";
 
 export const DesktopHeader = () => <div className="hidden lg:block ">
-<nav
- className="w-full flex justify-center items-center gap-4 bg-[#C5C6C7] h-20"
- >
+<nav className="w-full flex justify-center items-center gap-4 bg-[#C5C6C7] h-20">
+    <FlixnetSvg className="w-10 h-10"/>
     <Link href="/" className="text-2xl text-[#1F2833]">Home</Link>
     <Link href="/movies" className="text-2xl text-[#1F2833]">Movies</Link>
 </nav>
