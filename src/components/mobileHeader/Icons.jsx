10d939d02387c06bad9547bba02d6f0b39d52418
iiiulia12 <ratiuiulia@yahoo.com>
2024-07-10 00:32:20 +0300

    extract links and icons

---
+++ src/components/mobileHeader/Icons.jsx
@@ -0,0 +1,18 @@
+export const  CloseIcon = () => (
+    <svg
+    viewBox="0 0 12 12"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    className="text-[#1F2833] w-7 h-7"
+  >
+    <line x1="1" y1="7" x2="7" y2="1" stroke="black" strokeWidth="1.5" />
+    <line x1="1" y1="1" x2="7" y2="7" stroke="black" strokeWidth="1.5" />
+  </svg>
+
+  );
+
+  export const MenuIcon = () => (
+    <svg className="w-5 h-5 text-red-50" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+    </svg>
+  );
--- src/components/mobileHeader/links.jsx
+++ src/components/mobileHeader/links.jsx
@@ -1,5 +1,37 @@
 import Link from "next/link";
-export const Links = () => <div className="flex items-center flex-col gap-5">
-    <Link href="/" className="w-1/2 bg-slate-900 text-center h-12 flex items-center justify-center" onClick={() => setIsOpen(false)}>Home</Link>
-    <Link href="/movies" className="w-1/2 bg-slate-900 text-center  h-12 flex items-center justify-center" onClick={() => setIsOpen(false)}>Movies</Link>
-</div>
+
+export const NavLink = ({ href, children, onClick, additionalClasses }) => (
+    <Link href={href} className={`text-center flex items-center justify-center 
+    transition ease-in-out transform hover:-translate-y-1 ${additionalClasses}`} onClick={onClick}>
+      {children}
+    </Link>
+  );
+
+  export const LinksWrapper = ({ children, additionalClasses }) => (
+    <div className={`flex items-center  text-cyan-100 ${additionalClasses}`}>
+      {children}
+    </div>
+  );
+
+  export const MobileLinks = () => 
+  <LinksWrapper additionalClasses="flex-col gap-5">
+    <NavLink href="/" additionalClasses="w-1/2  h-12" onClick={() => setIsOpen(false)}>
+      Home
+    </NavLink>
+
+    <NavLink href="/movies" additionalClasses="w-1/2  h-12" onClick={() => setIsOpen(false)}>
+      Movies
+    </NavLink>
+
+ </LinksWrapper>
+
+export const DesktopLinks = () => 
+  <LinksWrapper additionalClasses="gap-5 px-10 ">
+      <NavLink href="/" additionalClasses="text-2xl  ">
+        Home
+      </NavLink>
+
+      <NavLink href="/movies" additionalClasses="text-2xl">
+        Movies
+      </NavLink>
+  </LinksWrapper>
