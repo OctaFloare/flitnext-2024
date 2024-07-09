2524256d849d6b6af99dc43c1a83b2630d702659
Cepoiu Radu Konstantin <radu.cepoiu@gmail.com>
2024-07-09 23:13:39 +0300

    added flixnet icon in mobile

--- src/components/mobileHeader/overlay.jsx
+++ src/components/mobileHeader/overlay.jsx
@@ -1,7 +1,8 @@
+import { FlixnetSvg } from "./flixnetSvg";
 import { BurgerSvg, XIconSvg } from "./xIconSvg";
 
 export const Overlay = ({ setIsOpen, isOpen }) => 
-<div className={`absolute left-0 top-0 h-full bg-[#C5C6C7] w-full ${isOpen ? "block" : "hidden"} z-10`}>
+<div className={`absolute left-0 top-0 h-full bg-[#C5C6C7] w-2/3 ${isOpen ? "block" : "hidden"} z-10`}>
     <button onClick={() => setIsOpen(false)}>
         <XIconSvg />
     </button>
