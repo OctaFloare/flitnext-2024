dc1c3183e61186e12019d1bc019c1218c096b72b
Hezanin <hezanina@gmail.com>
2024-07-10 17:33:37 +0300

    Update fetch movies hook to not include data transformation
    Update fetch movies hook to not include data transformation for movie data as it is redundant;
    Add error property to return object in useMovies hook for better error handling;

--- src/app/containers/hooks/useMovies.js
+++ src/app/containers/hooks/useMovies.js
@@ -1,27 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-export const useMovies = () => {
-  const fetchMovies = async () => {
-    try {
-      const response = await axios.get('/api/movies');
-      if (Array.isArray(response.data)) {
-        return response.data.map(movie => movie.name);
-      } else {
-        throw new Error('Unexpected response format');
-      }
-    } catch (error) {
-      throw new Error('Failed to fetch movies');
-    }
-  };
+const fetchMovies = async () => {
+  const response = await axios.get('/api/movies');
+  return response.data;
+};
 
-  const { data, isError, isLoading, isSuccess } = useQuery({
+export const useMovies = () => {
+  const { data, error, isError, isLoading, isSuccess } = useQuery({
     queryKey: ['getMovies'],
     queryFn: fetchMovies,
   });
 
   return {
     data,
+    error, 
     isError,
     isLoading,
     isSuccess,
--- src/app/containers/movies/content.jsx
+++ src/app/containers/movies/content.jsx
@@ -5,22 +5,22 @@ import { useMovies } from "../hooks/useMovies";
 import axios from "axios";
 
 export const Content = () => {
-  const { data : movieNames, isLoading, isError, isSuccess } = useMovies();
+  const { data, error, isError, isLoading, isSuccess} = useMovies();
 
   return (
     <div>
       <h1>Movies List</h1>
       <ul>
-        {isSuccess && movieNames.map((name, index) => (
-          <li key={index}>{name}</li>
+        {isSuccess && data.map(movie => (
+        <li key={movie.id}>{movie.name}</li>
         ))}
       </ul>
       {isError && (
         <div className="text-red-600">
-          Something went wrong with the movies fetch
+          {error.message}
         </div>
       )}
       {isLoading && <div>Content is loading...</div>}
-    </div>
+  </div>
   );
 };
