5b7d3ba4d87b46dacee1819b2059e4b9ee7ca2f7
vcosmin2701 <v.cosmin2701@gmail.com>
2024-07-11 19:58:49 +0300

    [customization] - Form customization using tailwind

--- src/app/containers/create-movie/index.jsx
+++ src/app/containers/create-movie/index.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useMutation } from "@tanstack/react-query";
+
 import axios from "axios";
 import MovieForm from "./MovieForm";
 
@@ -11,20 +12,29 @@ const createMovie = async (movieData) => {
 };
 
 export const CreateMovie = () => {
-  const { mutate, data, isSuccess, isError, error } = useMutation({
+  const { mutate, data, isSuccess, isError, error, reset } = useMutation({
     mutationFn: createMovie,
     mutationKey: ["create-movie"],
   });
 
   return (
     <>
-      {isError && <div className="text-red-600">{error.message}</div>}
-      <MovieForm mutate={mutate} />
+      {isError && (
+        <div className="fixed inset-x-0 bottom-0 z-50 flex items-center justify-center">
+          <div className="bg-red-500 text-white py-2 px-4 rounded-lg shadow-md">
+            <p className="text-lg font-bold">Error!</p>
+            <p className="text-sm">{error.message}</p>
+          </div>
+        </div>
+      )}
+      <MovieForm mutate={mutate} reset={reset} />
       {isSuccess && (
-        <div>
-          Newly created movie
-          <p>This is id: {data.id}</p>
-          <p>This is movie name: {data.name}</p>
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="bg-green-500 text-white py-2 px-4 rounded-lg shadow-md">
+            <p className="text-lg font-bold">Newly created movie</p>
+            <p className="text-sm">This is id: {data.id}</p>
+            <p className="text-sm">This is movie name: {data.name}</p>
+          </div>
         </div>
       )}
     </>
