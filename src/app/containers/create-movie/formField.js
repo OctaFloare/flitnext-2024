846c45f6ee3f0824fe6be6805f3e90199466f0b7
Hezanin <hezanina@gmail.com>
2024-07-12 21:34:21 +0300

    Update form field to implement formik field
    Update form field to contain a formik field as to remove the redundant custom implementation;

--- src/app/containers/create-movie/formField.js
+++ src/app/containers/create-movie/formField.js
@@ -1,21 +1,10 @@
+
 import React from 'react';
+import { Field, ErrorMessage } from 'formik';
 
-const FormField = ({ id, label, formik }) => (
-  <div className="formGroup">
-    <label className='formLabel' htmlFor={id}>{label}</label>
-    <input
-      id={id}
-      name={id}
-      type="text"
-      onChange={formik.handleChange}
-      onBlur={formik.handleBlur}
-      value={formik.values[id]}
-      className="input"
-    />
-    {formik.touched[id] && formik.errors[id] ? (
-      <div className="text-red-500 text-sm mt-1">{formik.errors[id]}</div>
-    ) : null}
-  </div>
-);
-
-export default FormField;
+export const FormField = ({ label, name, type = 'text' }) => 
+<div className="formGroup">
+    <label className='formLabel' htmlFor={name}>{label}</label>
+    <Field className="input" type={type} id={name} name={name}/>
+    <ErrorMessage name={name} component="div" style={{ color: 'red' }} />
+</div>
