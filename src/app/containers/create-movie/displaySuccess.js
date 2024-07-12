6c63366ce437096d199fa5b5aafd3880fbd91fc9
Hezanin <hezanina@gmail.com>
2024-07-12 22:26:05 +0300

    Code clean up and minor refactoring
    - remove reduntant rename of mutate function and call
    - update display error component to not receive error object but error message
    - update display success component to destructure id and name
    - update package file dependencies to not include '^' before version

--- src/app/containers/create-movie/displaySuccess.js
+++ src/app/containers/create-movie/displaySuccess.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-const DisplaySuccess = ({ data }) => {
+const DisplaySuccess = ({ data: {id, name} }) => {
   const [visible, setVisible] = useState(true);
 
   useEffect(() => {
@@ -16,8 +16,8 @@ const DisplaySuccess = ({ data }) => {
   return (
     <div className="absolute bottom-0 bg-green-100 border border-green-500 text-green-700 rounded p-4 w-full max-w-md text-center mt-4">
       <h3 className="font-bold mb-2">Newly Created Movie</h3>
-      <p><strong>ID:</strong> {data.id}</p>
-      <p><strong>Movie Name:</strong> {data.name}</p>
+      <p><strong>ID:</strong> {id}</p>
+      <p><strong>Movie Name:</strong> {name}</p>
     </div>
   );
 };
--- src/app/containers/create-movie/index.jsx
+++ src/app/containers/create-movie/index.jsx
@@ -6,15 +6,11 @@ import DisplayError from "./displayError";
 import DisplaySuccess from "./displaySuccess";
 
 export const CreateMovie = () => {
-    const { mutate, data, isSuccess, isError, error } = useCreateMovie();
-
-    const handleFormSubmit = (values) => {
-      mutate(values);
-    };
+    const { mutate : handleFormSubmit, data, isSuccess, isError, error } = useCreateMovie();
       
     return (
         <div className="flex flex-col items-center justify-center h-full w-full space-y-4">
-          {isError && <DisplayError error={error} />}
+          {isError && <DisplayError error={error.message} />}
           <CreateMovieForm onSubmit={handleFormSubmit} />
           {isSuccess && <DisplaySuccess data={data} />}
         </div>
